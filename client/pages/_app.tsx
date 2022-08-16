e993d298981eea4fd361948130538ea92ca4be47
Mohamed Fathy <60598620+Mohamed-Fathy-Salah@users.noreply.github.com>
2022-08-16 09:21:44 +0200

    Revert "Front setup"

--- client/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import DevNote from '../components/DevNote'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <><DevNote /><Component {...pageProps} /></>
-}
-
-export default MyApp
