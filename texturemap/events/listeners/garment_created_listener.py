70a5240651bd5ac50e0c73dbb8afece95366e16f
Mohamed Fathy <mofasa.peace@gmail.com>
2022-08-22 18:50:31 +0200

    texture listens to garment created & updated

--- texturemap/events/listeners/garment_created_listener.py
+++
@@ -1,8 +0,0 @@
-async def garment_created_listener(msg):
-    print("Received a message (seq={}): {}".format(msg.seq, msg.data))
-    #todo
-    # get front and back images - garment class - gender
-    # assure garment class in enum
-    # assure gender in enum
-    # run ../../contours.py
-    # publish event to subject texturemap:created ->  await sc.publish(subject, b'hello')
--- texturemap/natsWrapper.py
+++ texturemap/natsWrapper.py
@@ -1,8 +1,8 @@
 from nats.aio.client import Client as NATS
 from stan.aio.client import Client as STAN
-# from .types import Subjects
 from os import environ
 from events.listeners.garment_created_listener import garment_created_listener
+import json
 
 cluster_id = environ.get('NATS_CLUSTER_ID') 
 client_id = environ.get('NATS_CLIENT_ID')
@@ -27,14 +27,42 @@ async def run(loop):
     sc = STAN()
     await sc.connect(cluster_id, client_id, nats=nc, loop=loop)
 
-    await sc.subscribe("garment:created", cb=garment_created_listener)
-    for i in range(10):
-        print("send msg")
-        await sc.publish("garment:created", b'hello')
+    async def garment_created_listener(msg):
+        print("Received a message (seq={}): {}".format(msg.seq, msg.data))
+        data = json.loads(msg.data)
+        garmentClass = data['garmentClass']
+        gender = data['gender']
+        frontPhoto = data['frontPhoto']
+        backPhoto = data['backPhoto']
+        print(garmentClass, gender)
+        #todo : run ../../contours.py
+        await sc.publish("texturemap:created", b'created')
 
-# def connect():
-    # check_environment_vars()
-    # loop = asyncio.new_event_loop()
-    # asyncio.set_event_loop(loop)
-    # loop.run_until_complete(run(loop))
-    # loop.close()
+    async def garment_updated_listener(msg):
+        print("Received a message (seq={}): {}".format(msg.seq, msg.data))
+        data = json.loads(msg.data)
+        garmentClass = data['garmentClass']
+        gender = data['gender']
+        frontPhoto = data['frontPhoto']
+        backPhoto = data['backPhoto']
+        print(garmentClass, gender)
+        #todo : run ../../contours.py
+        await sc.publish("texturemap:created", b'updated')
+
+    await sc.subscribe("garment:created", cb=garment_created_listener)
+    await sc.subscribe("garment:updated", cb=garment_updated_listener)
+
+# import base64
+# from PIL import Image
+# import cv2
+# from StringIO import StringIO
+# import numpy as np
+
+# def readb64(base64_string):
+    # sbuf = StringIO()
+    # sbuf.write(base64.b64decode(base64_string))
+    # pimg = Image.open(sbuf)
+    # return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+# cvimg = readb64('R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7')
+# cv2.imshow(cvimg)
