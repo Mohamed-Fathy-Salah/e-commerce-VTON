85fd35fb9eeb1fe1d11f3d28e67f87e9d0966245
Mohamed Fathy <mofasa.peace@gmail.com>
2022-08-25 17:18:33 +0200

    nats options

--- body-garment/natsWrapper.py
+++ body-garment/natsWrapper.py
@@ -4,14 +4,11 @@ from nats.aio.client import Client as NATS
 from stan.aio.client import Client as STAN
 from os import environ
 import json
-import logging
+import logging as log
 from db import get_session
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.customers import Customers
-
-log = logging.getLogger()
-log.addHandler(logging.StreamHandler())
 
 cluster_id = environ.get('NATS_CLUSTER_ID') 
 client_id = environ.get('NATS_CLIENT_ID')
@@ -37,48 +34,49 @@ async def run(loop):
     # {customerId: string, name: string, age: number, gender: Gender, version:number}
     async def customer_data_created_listener(msg, session: AsyncSession = Depends(get_session)):
         data = json.loads(msg.data)
-        log.warning(data)
+        log.warning(f"customer created {data}")
         customer = Customers(id= data['customerId'], gender= data['gender'], skin= 'white')
 
         session.add(customer)
         await session.commit()
         await session.refresh(customer)
         await sc.ack(msg)
-        return customer
 
     async def customer_data_updated_listener(msg):
         data = json.loads(msg.data)
         # with Session(DB().engine) as session:
             # session.get()
-        log.warning(data)
+        log.warning(f" customer updated {data}")
         await sc.ack(msg)
 
     async def garment_created_listener(msg):
         data = json.loads(msg.data)
-        log.warning(data)
+        log.warning(f" garment created {data}")
         await sc.ack(msg)
 
     async def garment_updated_listener(msg):
         data = json.loads(msg.data)
-        log.warning(data)
+        log.warning(f" garment updated {data}")
         await sc.ack(msg)
 
     async def garment_deleted_listener(msg):
         data = json.loads(msg.data)
-        log.warning(data)
+        log.warning(f" garment deleted {data}")
         await sc.ack(msg)
 
     async def texturemap_created_listener(msg):
         data = json.loads(msg.data)
-        log.warning(data)
+        log.warning(f"texturemap created {data}")
         await sc.ack(msg)
 
-    await sc.subscribe("customer:data:created", cb=customer_data_created_listener)
-    await sc.subscribe("customer:data:updated", cb=customer_data_updated_listener)
-    await sc.subscribe("garment:created", cb=garment_created_listener)
-    await sc.subscribe("garment:updated", cb=garment_updated_listener)
-    await sc.subscribe("garment:deleted", cb=garment_deleted_listener)
-    await sc.subscribe("texturemap:created", cb=texturemap_created_listener)
+    queueGroupName = "bodygarment-service"
+
+    await sc.subscribe("customer:data:created", cb=customer_data_created_listener, deliver_all_available=True, manual_acks=True, ack_wait=True, durable_name= queueGroupName)
+    await sc.subscribe("customer:data:updated", cb=customer_data_updated_listener, deliver_all_available=True, manual_acks=True, ack_wait=True, durable_name= queueGroupName)
+    await sc.subscribe("garment:created", cb=garment_created_listener, deliver_all_available=True, manual_acks=True, ack_wait=True, durable_name= queueGroupName)
+    await sc.subscribe("garment:updated", cb=garment_updated_listener, deliver_all_available=True, manual_acks=True, ack_wait=True, durable_name= queueGroupName)
+    await sc.subscribe("garment:deleted", cb=garment_deleted_listener, deliver_all_available=True, manual_acks=True, ack_wait=True, durable_name= queueGroupName)
+    await sc.subscribe("texturemap:created", cb=texturemap_created_listener, deliver_all_available=True, manual_acks=True, ack_wait=True, durable_name= queueGroupName)
 
 def connect():
     check_environment_vars()
