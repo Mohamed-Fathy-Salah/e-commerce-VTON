38ddd8463f45bb3778e8723569e4108ee6d33135
Mohamed Fathy <mofasa.peace@gmail.com>
2022-09-01 17:31:22 +0200

    add measurements to customers & sizeprefs

--- customers/src/routes/__test__/update.test.ts
+++ customers/src/routes/__test__/update.test.ts
@@ -1,6 +1,12 @@
 import { app } from "../../app";
 import request from "supertest";
-import { GarmentClass, GarmentSize, Gender, SkinTone, UserType } from "@mfsvton/common";
+import {
+  GarmentClass,
+  GarmentSize,
+  Gender,
+  SkinTone,
+  UserType,
+} from "@mfsvton/common";
 import mongoose from "mongoose";
 
 it("error if user is not signed in ", async () => {
@@ -19,14 +25,99 @@ it("error if user is not in DB ", async () => {
   await request(app)
     .put("/api/customerdata")
     .set("Cookie", global.signin(UserType.Customer))
-    .send({
-      name : 'hihi',
-      gender: Gender.Male,
-      age: 15,
-      skinTone: SkinTone.White,
-      sizePreferences: [{ garmentClass: GarmentClass.Shirt, garmentSize: GarmentSize.Large }]
-    })
+    .field("name", "hihi")
+    .field("gender", Gender.Male)
+    .field("age", 15)
+    .field("skinTone", SkinTone.White)
+    .field(
+      "sizePreferences",
+      JSON.stringify([
+        { garmentClass: GarmentClass.Shirt, garmentSize: GarmentSize.Large },
+      ])
+    )
     .expect(404);
+});
+
+it("error if wrong data", async () => {
+  const customerId = new mongoose.Types.ObjectId().toHexString();
+  const cookie = global.signin(UserType.Customer, customerId);
+
+  await request(app)
+    .post("/api/customerdata")
+    .set("Cookie", cookie)
+    .send({
+      name: "blah",
+      age: 15,
+      customerId,
+      gender: Gender.Male,
+    })
+    .expect(201);
+
+  await request(app)
+    .put("/api/customerdata")
+    .set("Cookie", cookie)
+    .field("name", "hihi")
+    .field("gendes", Gender.Male)
+    .field("age", 15)
+    .field("skinTone", SkinTone.White)
+    .field(
+      "sizePreferences",
+      JSON.stringify([
+        { garmentClass: GarmentClass.Shirt, garmentSize: GarmentSize.Large },
+        { GarmentClass: "shit" },
+      ])
+    )
+    .expect(400);
+
+  await request(app)
+    .put("/api/customerdata")
+    .set("Cookie", cookie)
+    .field("name", "hihi")
+    .field("gendes", Gender.Male)
+    .field("age", 15)
+    .field("skinTone", SkinTone.White)
+    .field(
+      "sizePreferences",
+      JSON.stringify([
+        { garmentClass: GarmentClass.Shirt, garmentSize: GarmentSize.Large },
+      ])
+    )
+    .field(
+      "measurements",
+      JSON.stringify({
+        weight: 100,
+        chest: 100,
+        waist: 100,
+        hips: 100,
+        arm: 100,
+        inseam: 100,
+        neckline: 100,
+        shoulder: 100,
+      })
+    )
+    .expect(400);
+
+  await request(app)
+    .put("/api/customerdata")
+    .set("Cookie", cookie)
+    .field("name", "hihi")
+    .field("gendes", Gender.Male)
+    .field("age", 15)
+    .field("skinTone", SkinTone.White)
+    .field(
+      "measurements",
+      JSON.stringify({
+        height: 100,
+        weight: 100,
+        waist: 100,
+        hips: 100,
+        arm: 100,
+        inseam: 100,
+        neckline: 100,
+        shoulder: 100,
+      })
+    )
+    .expect(400);
 });
 
 it("correct data when user signin", async () => {
@@ -40,28 +131,95 @@ it("correct data when user signin", async () => {
       name: "blah",
       age: 15,
       customerId,
-      gender: Gender.Male
+      gender: Gender.Male,
     })
     .expect(201);
 
   const res = await request(app)
-  .put('/api/customerdata')
-  .set('Cookie', cookie)
-  .send({
-      name : 'hihi',
-      gender: Gender.Male,
-      age: 15,
-      skinTone: SkinTone.White,
-      sizePreferences: [{ garmentClass: GarmentClass.Shirt, garmentSize: GarmentSize.Large }]
-    })
-  .expect(201)
+    .put("/api/customerdata")
+    .set("Cookie", cookie)
+    .field("name", "hihi")
+    .field("gender", Gender.Male)
+    .field("age", 15)
+    .field("skinTone", SkinTone.White)
+    .field(
+      "measurements",
+      JSON.stringify({
+        height: 150,
+        chest: 100,
+        weight: 100,
+        waist: 100,
+        hips: 100,
+        arm: 60,
+        inseam: 100,
+        neckline: 50,
+        shoulder: 50,
+      })
+    )
+    .expect(201);
 
   expect(res.body.customerId).toEqual(customerId);
-  expect(res.body.name).toEqual('hihi');
+  expect(res.body.name).toEqual("hihi");
   expect(res.body.gender).toEqual(Gender.Male);
   expect(res.body.age).toEqual(15);
   expect(res.body.skinTone).toEqual(SkinTone.White);
-  expect(res.body.sizePreferences.length).toEqual(1);
+  expect(res.body.sizePreferences.length).toEqual(0);
+  expect(res.body.measurements.height).toEqual(150);
 });
 
-it.todo('add measurements and append-modify size preferences');
+it("append-modify size preferences", async () => {
+  const customerId = new mongoose.Types.ObjectId().toHexString();
+  const cookie = global.signin(UserType.Customer, customerId);
+
+  await request(app)
+    .post("/api/customerdata")
+    .set("Cookie", cookie)
+    .send({
+      name: "blah",
+      age: 15,
+      customerId,
+      gender: Gender.Male,
+    })
+    .expect(201);
+
+    await request(app)
+    .put("/api/customerdata")
+    .set("Cookie", cookie)
+    .field("name", "hihi")
+    .field("gender", Gender.Male)
+    .field("age", 15)
+    .field("skinTone", SkinTone.White)
+    .field(
+      "sizePreferences",
+      JSON.stringify({})
+    )
+    .expect(400);
+
+    await request(app)
+    .put("/api/customerdata")
+    .set("Cookie", cookie)
+    .field("name", "hihi")
+    .field("gender", Gender.Male)
+    .field("age", 15)
+    .field("skinTone", SkinTone.White)
+    .field(
+      "sizePreferences",
+      JSON.stringify([])
+    )
+    .expect(201);
+
+    const res = await request(app)
+    .put("/api/customerdata")
+    .set("Cookie", cookie)
+    .field("name", "hihi")
+    .field("gender", Gender.Male)
+    .field("age", 15)
+    .field("skinTone", SkinTone.White)
+    .field(
+      "sizePreferences",
+      JSON.stringify([{garmentClass: GarmentClass.Shirt, garmentSize: GarmentSize.Large}, {garmentClass: GarmentClass.Shirt, garmentSize: GarmentSize.Small},{garmentClass: GarmentClass.Pant, garmentSize: GarmentSize.Medium}])
+    )
+    .expect(201);
+
+    expect(res.body.sizePreferences.length).toEqual(2);
+});
--- customers/src/routes/update.ts
+++ customers/src/routes/update.ts
@@ -1,5 +1,7 @@
 import {
-    Gender,
+  GarmentClass,
+  GarmentSize,
+  Gender,
   NotFoundError,
   requireCustomerAuth,
   SkinTone,
@@ -10,29 +12,70 @@ import { body } from "express-validator";
 import { CustomerDataUpdatedPublisher } from "../events/publishers/customer-data-updated-publisher";
 import { Customer } from "../models/customer";
 import { natsWrapper } from "../nats-wrapper";
-import multer from 'multer';
+import multer from "multer";
 
 const router = express.Router();
 
-const Storage = multer.memoryStorage()
+const Storage = multer.memoryStorage();
 
-const upload = multer({ storage:Storage });
+const upload = multer({ storage: Storage });
 
 router.put(
   "/api/customerdata",
   requireCustomerAuth,
-  upload.single('file'),
+  upload.single("file"),
   [
-    body("name").custom((value) => {
-      return value.length > 0;
+    body("name").notEmpty(),
+    body("gender").custom((value) => Object.values(Gender).includes(value)),
+    body("age").custom((value) => value > 13 && value < 120),
+    body("skinTone").custom((value) => Object.values(SkinTone).includes(value)),
+    body("measurements").custom((value) => {
+      if (!value) return true;
+
+      try {
+        const { height, arm, hips, weight, chest, waist, inseam, neckline, shoulder, } = JSON.parse(value);
+
+        return (
+          height && arm && hips && weight && chest && waist && inseam && neckline && shoulder &&
+          height >= 140 && height <= 250 &&
+          arm >= 50 && arm <= 80 &&
+          hips >= 80 && hips <= 120 &&
+          weight >= 40 && weight <= 130 &&
+          chest >= 80 && chest <= 130 &&
+          waist >= 80 && waist <= 130 &&
+          inseam >= 60 && inseam <= 100 &&
+          neckline >= 35 && neckline <= 60 &&
+          shoulder >= 30 && shoulder <= 70
+        );
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
     }),
-  body("gender").custom(value => Object.values(Gender).includes(value)),
-  body("age").custom(value => value > 13 && value < 120),
-  body("skinTone").custom(value => Object.values(SkinTone).includes(value)),
-  // todo: validate every field 
-  //body("measurements").custom((value) => { return !value || ;}),
-  //body("photo").custom((value) => { return !value || ;}),
-  //body("sizePreferences").custom((value) => { return !value || ;}),
+    body("sizePreferences").custom((value) => {
+      if(!value) return true;
+
+      try {
+        const arr = JSON.parse(value);
+
+        for (const obj of arr) {
+          const { garmentClass, garmentSize } = obj;
+
+          if (
+            !garmentClass ||
+            !garmentSize ||
+            !Object.values(GarmentClass).includes(garmentClass) ||
+            !Object.values(GarmentSize).includes(garmentSize)
+          )
+            return false;
+        }
+
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    }),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -43,29 +86,43 @@ router.put(
       throw new NotFoundError();
     }
 
-    customer.set({name: req.body.name || customer.name});
-    customer.set({gender: req.body.gender || customer.gender});
-    customer.set({age: req.body.age || customer.age});
-    customer.set({skinTone: req.body.skinTone || customer.skinTone});
-    customer.set({measurements: req.body.measurements});
-    // todo: make size prefs unique by size
-    customer.set({sizePreferences: req.body.sizePreferences});
-    if(req.file) {
-        customer.set({photo: req.file.buffer.toString('base64')});
+    customer.set({ name: req.body.name || customer.name });
+    customer.set({ gender: req.body.gender || customer.gender });
+    customer.set({ age: req.body.age || customer.age });
+    customer.set({ skinTone: req.body.skinTone || customer.skinTone });
+
+    if (req.body.measurements) {
+      customer.set({ measurements: JSON.parse(req.body.measurements) });
+    }
+
+    if (req.body.sizePreferences) {
+      const mp = new Map<string, string>();
+      const reqSP = JSON.parse(req.body.sizePreferences);
+      for (const obj of reqSP) mp.set(obj.garmentClass, obj.garmentSize);
+
+      const arr: { garmentClass: string; garmentSize: string }[] = [];
+      for (const [key, val] of mp.entries())
+        arr.push({ garmentClass: key, garmentSize: val });
+
+      customer.set({ sizePreferences: arr });
+    }
+
+    if (req.file) {
+      customer.set({ photo: req.file.buffer.toString("base64") });
     }
 
     await customer.save();
 
     new CustomerDataUpdatedPublisher(natsWrapper.client).publish({
-        customerId: customer.customerId,
-        name: customer.name,
-        gender: customer.gender,
-        age: customer.age,
-        skin: customer.skinTone,
-        measurements: customer.measurements,
-        sizePreferences: customer.sizePreferences,
-        photo: customer.photo,
-        version: customer.version
+      customerId: customer.customerId,
+      name: customer.name,
+      gender: customer.gender,
+      age: customer.age,
+      skin: customer.skinTone,
+      measurements: customer.measurements,
+      sizePreferences: customer.sizePreferences,
+      photo: customer.photo,
+      version: customer.version,
     });
 
     res.status(201).send(customer);
