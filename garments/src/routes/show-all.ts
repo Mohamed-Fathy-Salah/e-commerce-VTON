ad07391a7241898877958e265c4e402b585e56d4
Mohamed Fathy <mofasa.peace@gmail.com>
2022-09-05 19:11:48 +0200

    wait all garments for cart

--- garments/src/routes/show-all.ts
+++ garments/src/routes/show-all.ts
@@ -23,7 +23,9 @@ router.get(
 
     const cart = JSON.parse(Buffer.from(encodedCart, 'base64').toString('ascii'));
 
-    const garments = cart.map(async (value: {garmentId: string}) => await Garment.findById(value.garmentId))
+    let garments = cart.map((value: {garmentId: string}) => Garment.findById(value.garmentId))
+
+    garments = await Promise.allSettled(garments);
 
     res.status(200).send(garments)
 });
