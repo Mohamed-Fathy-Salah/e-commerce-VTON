3e21c204785b152898dc1e29404e5f362c37409e
Mohamed Fathy <mofasa.peace@gmail.com>
2022-10-04 11:15:47 +0200

    admins listens to payment created

---
+++ admins/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,22 @@
+import { Listener, NotFoundError, OrderStatus, PaymentCreatedEvent, Subjects } from "@mfsvton/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/orders";
+import {queueGroupName} from './queue-group-name'
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+    subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+    queueGroupName = queueGroupName;
+    async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
+        const order = await Order.findById(data.orderId);
+
+        if(!order) {
+            throw new NotFoundError();
+        }
+
+        order.set({status: OrderStatus.Completed});
+        order.save();
+
+        msg.ack();
+    }
+
+}
